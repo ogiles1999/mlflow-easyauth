5635819a9554cd5f32f966a1be0a7d2d2d875171
Oliver Giles <ogiles@email.com>
2022-09-26 18:02:32 +0100

    modified .sh

--- mlflow.sh
+++ mlflow.sh
@@ -1,8 +1,4 @@
 #!/bin/bash -x
-
-# FIXME: write google credentials to disk from envvar
-
-
 if [[ -z "${ARTIFACT_URL}" ]]; then
     export ARTIFACT_URL="mlruns"
 fi
@@ -11,10 +7,8 @@ if [[ -z "${DATABASE_URL}" ]]; then
     export DATABASE_URL="./mlruns"
 fi
 
-# Fix naming convention mismatch between Heroku and mlflow
-DATABASE_URL=${DATABASE_URL//postgres:/postgresql:} 
+DATABASE_URL=${DATABASE_URL//postgres:/postgresql:}
 
-# Credentials for Google Cloud Storage
 if [[ -z "${GOOGLE_APPLICATION_CREDENTIALS}" ]]; then
     export GOOGLE_APPLICATION_CREDENTIALS=/app/credentials.json
 fi
